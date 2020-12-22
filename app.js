c63f912ed1dedbbd4c1bc02dce2c85fca6bd738f
AliouBahSchool <76268429+AliouBahSchool@users.noreply.github.com>
2020-12-22 08:48:56 -0500

    fixed dos hole

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 const app = express();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
 
+// apply rate limiter to all requests
+app.use(limiter);
 //body parser setup
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
